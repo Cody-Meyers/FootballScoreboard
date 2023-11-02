2a57548e3b44d4e358240b70e039629a39d64969
Cody Meyers <cody.s.meyers@gmail.com>
2023-11-01 17:33:44 -0700

    added html scoring buttons - added Coin element creation via JS with Class
    need to still add animation functionality to the coins, the 'addition' of existing score to scoreboard section, then the removal of the added Div to 'reset' it for the next score addition

--- gridIndex.js
+++ gridIndex.js
@@ -95,3 +95,90 @@ function tickerHomeTransformation() {
   element.classList.toggle("header-ticker-transform");
   element.classList.toggle("transform-hidden");
 }
+
+// !Below are Coin Transformation attempts -- need to add this functionality AFTER the creation of the Coin Div function: visitorScoreCoin -- needs to remove DIV after animation completes
+// const scoreCoinVisitorTransformButton = document.querySelector("#button-score-coin-visitor-transform");
+// scoreCoinVisitorTransformButton.addEventListener("click" scoreVisitorTransformation);
+
+// function scoreVisitorTransformation() {
+//   const element = document.querySelector(".visitor-score-coin");
+//   element.classList.toggle(".transform-hidden-coin");
+//   element.classList.toggle(".visitor-coin-transform");
+// }
+
+// const element = document.querySelector("#button-score-coin-visitor-transform");
+// element.addEventListener("animationstart", listener, false);
+// element.addEventListener("animationend", listener, false);
+// element.addEventListener("animationiteration", listener, false);
+
+// element.className = "coin-easeOutBounce";
+
+// function listener(event) {
+//   const e = document.createElement("p");
+//   e.textContent = "";
+// }
+
+// function scoreCoinValue(score) {
+//   document.getElementById("visitor-score-coin-id").innerHTML = score;
+// }
+
+// function scoreChange(visitorScoreValue, homeScoreValue) {
+//   const eVisitorScoreCoin = document.getElementbyID("visitor-score-coin-id");
+//   const eHomeScoreCoin = document.getElementbyID("home-score-coin-id");
+//   const eVisitorScore = document.getElementbyID("visitor-score-id");
+//   const eHomeScore = document.getElementbyID("home-score-id");
+
+//   eVisitorScoreCoin.innerHTML = visitorScoreValue;
+//   eVisitorScore.innerHTML = `evisitorScore.innerHTML + visitorScoreValue`;
+//   eHomeScoreCoin.innerHTML = homeScoreValue;
+//   eHomeScore.innerHTML = homeScoreValue;
+// }
+
+// const scoreCoinVisitorTransformButtonFieldGoal = document.querySelector(
+//   "#button-score-coin-visitor-transform"
+// );
+// scoreCoinVisitorTransformButtonFieldGoal.addEventListener(
+//   "click",
+//   visitorScoreCoin
+// );
+
+// function visitorScoreCoin(e) {
+//   const eVisitorScoreCoin = document.createElement("div");
+//   const eVisitorScoreValue = document.createTextNode(e);
+
+//   eVisitorScoreCoin.appendChild(eVisitorScoreValue);
+
+//   document
+//     .getElementById("visitor-score-coin-id")
+//     .appendChild(eVisitorScoreCoin);
+// }
+
+function visitorScoreCoin(e) {
+  const eVisitorScoreCoin = document.createElement("div");
+  const eVisitorScoreCoinValue = document.createTextNode(e);
+
+  document
+    .getElementById("visitor-score-coin-id")
+    .appendChild(eVisitorScoreCoin)
+    .classList.add("visitor-score-coin-transform-class");
+
+  eVisitorScoreCoin.appendChild(eVisitorScoreCoinValue);
+
+  document
+    .getElementById("visitor-score-coin-id")
+    .appendChild(eVisitorScoreCoin);
+}
+
+function homeScoreCoin(e) {
+  const eHomeScoreCoin = document.createElement("div");
+  const eHomeScoreCoinValue = document.createTextNode(e);
+
+  document
+    .getElementById("home-score-coin-id")
+    .appendChild(eHomeScoreCoin)
+    .classList.add("home-score-coin-transform-class");
+
+  eHomeScoreCoin.appendChild(eHomeScoreCoinValue);
+
+  document.getElementById("home-score-coin-id").appendChild(eHomeScoreCoin);
+}
