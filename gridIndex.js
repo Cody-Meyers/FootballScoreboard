f51ed5af7e1a795e3e39e059a7ce0c28b7c37128
Cody Meyers <cody.s.meyers@gmail.com>
2023-11-05 23:57:17 -0800

    Created tickerTransformation function to handle all 3 tickers

--- gridIndex.js
+++ gridIndex.js
@@ -23,55 +23,23 @@ function asideHomeTransformation() {
   element.classList.toggle("transform-hidden");
 }
 
-const tickerVisitorTransformButton = document.querySelector(
-  "#button-ticker-v-transform"
-);
-tickerVisitorTransformButton.addEventListener(
-  "click",
-  tickerVisitorTransformation
-);
+// Ticker Transformation/Visibility Function - Visitor/Center/Home
+function tickerTransformation(ticker1, ticker2, ticker3) {
+  const e1 = document.querySelector(ticker1);
+  const e2 = document.querySelector(ticker2);
+  const e3 = document.querySelector(ticker3);
+  const e2Test = e2.classList.contains("transform-hidden");
+  const e3Test = e3.classList.contains("transform-hidden");
 
-function tickerVisitorTransformation() {
-  const element = document.querySelector(".header-ticker-visitor");
-  element.classList.toggle("header-ticker-transform");
-  element.classList.toggle("transform-hidden");
-}
-
-const tickerCenterTransformButton = document.querySelector(
-  "#button-ticker-center-transform"
-);
-tickerCenterTransformButton.addEventListener(
-  "click",
-  tickerCenterTransformation
-);
-
-function tickerCenterTransformation() {
-  const element = document.querySelector(".header-ticker-center");
-  const eV = document.querySelector(".header-ticker-visitor");
-  const eH = document.querySelector(".header-ticker-home");
-  const eVTest = eV.classList.contains(".transform-hidden");
-  const eHTest = eV.classList.contains(".transform-hidden");
-
-  if (!(eVTest || eHTest)) {
-    eV.classList.add("transform-hidden");
-    eH.classList.add("transform-hidden");
-    element.classList.toggle("header-ticker-transform");
-    element.classList.toggle("transform-hidden");
+  if (!e2Test || !e3Test) {
+    e2.classList.add("transform-hidden");
+    e3.classList.add("transform-hidden");
+    e1.classList.toggle("header-ticker-transform");
+    e1.classList.toggle("transform-hidden");
   } else {
-    element.classList.toggle("header-ticker-transform");
-    element.classList.toggle("transform-hidden");
+    e1.classList.toggle("header-ticker-transform");
+    e1.classList.toggle("transform-hidden");
   }
-}
-
-const tickerHomeTransformButton = document.querySelector(
-  "#button-ticker-h-transform"
-);
-tickerHomeTransformButton.addEventListener("click", tickerHomeTransformation);
-
-function tickerHomeTransformation() {
-  const element = document.querySelector(".header-ticker-home");
-  element.classList.toggle("header-ticker-transform");
-  element.classList.toggle("transform-hidden");
 }
 
 // Reset Score
@@ -144,164 +112,30 @@ function addPointsToScore(points, side) {
   document.querySelector(side).append(newScore);
 }
 
-// ---Select Coin at End of Coin Animation then call function to remove Coin DIV
-// function coinAnimationEndFunction() {
-//   const animationEndDivRemove = document.querySelector(".coin-transform");
-//   animationEndDivRemove.addEventListener("animationend", scoreCoinRemove);
-// }
-// ----Remove Coin DIV
-// function scoreCoinRemove() {
-//   const coinDiv = document.querySelector(".coin-transform");
-//   coinDiv.remove(coinDiv);
-// }
-
-// function homeScoreCoinRemove() {
-//   const eHomeScoreCoinDivParent = document.getElementById("home-score-coin-id");
-
-//   if (eHomeScoreCoinDivParent.hasChildNodes()) {
-//     eHomeScoreCoinDivParent.removeChild(eHomeScoreCoinDivParent.children[0]);
-//   }
-// }
-
-// --Visitor Coin DIV Creation
-// function visitorScoreCoin(e) {
-//   const eVisitorScoreCoin = document.createElement("div");
-//   const eVisitorScoreCoinValue = document.createTextNode(e);
-
-//   document
-//     .getElementById("visitor-score-coin-id")
-//     .appendChild(eVisitorScoreCoin)
-//     .classList.add("visitor-score-coin-transform-class", "coin-transform");
-
-//   eVisitorScoreCoin.appendChild(eVisitorScoreCoinValue);
-
-//   document
-//     .getElementById("visitor-score-coin-id")
-//     .appendChild(eVisitorScoreCoin);
-// }
-
-// --Home Coin DIV Creation
-// function homeScoreCoin(e) {
-//   const eHomeScoreCoin = document.createElement("div");
-//   const eHomeScoreCoinValue = document.createTextNode(e);
-
-//   document
-//     .getElementById("home-score-coin-id")
-//     .appendChild(eHomeScoreCoin)
-//     .classList.add("home-score-coin-transform-class", "coin-transform");
-
-//   eHomeScoreCoin.appendChild(eHomeScoreCoinValue);
-
-//   document.getElementById("home-score-coin-id").appendChild(eHomeScoreCoin);
-// }
-
-// const API_URL = "http://localhost:3500";
-
-// fetch("http://localhost:3500/scores", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify({
-//     id: 2,
-//     "score-visitor": 2,
-//     points: 1,
-//   }),
-// })
+// fetch("http://localhost:3500/scores")
 //   .then((response) => response.json())
-//   .then((response) => console.log(response.status));
-
-fetch("http://localhost:3500/scores")
-  .then((response) => response.json())
-  .then((response) => console.log(response));
-
-fetch("http://localhost:3500/scores")
-  .then((response) => response.json())
-  .then((response) => console.log(JSON.stringify(response)));
+//   .then((response) => console.log(response));
 
 // fetch("http://localhost:3500/scores")
-// .then((response) => response.json()).map(scores.points)
+//   .then((response) => response.json())
+//   .then((response) => console.log(JSON.stringify(response)));
 
-// let visitorScoring = document.getElementById("visitor-score-points");
+// // fetch("http://localhost:3500/scores")
+// // .then((response) => response.json()).map(scores.points)
 
-getScores = async () => {
-  const visitorScoring = document.getElementById("visitor-score-points");
-  fetch("http://localhost:3500/scores/1")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(JSON.stringify(data));
-      visitorScoring.innerHTML(`${data.score}`);
-      // data.forEach((x) => {
-      //   console.log(data);
-      //   visitorScoring.innerHTML(`<h2>${x.score}</h2>`);
-    });
-};
-// const visitorScoring = document.getElementById("visitor-score-points");
-// const objToParse = JSON.parse(grabScore.map());
-// visitorScoring.innerHTML = objToParse.score;
+// // let visitorScoring = document.getElementById("visitor-score-points");
 
-// getScores = async (x) => {
-//   const grabScore = await fetch("http://localhost:3500/visitorScore").then(
-//     (response) => response.json()
-//   );
+// getScores = async () => {
 //   const visitorScoring = document.getElementById("visitor-score-points");
-//   const objToParse = JSON.parse(grabScore.map());
-//   visitorScoring.innerHTML = objToParse.score;
+//   fetch("http://localhost:3500/scores/1")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(JSON.stringify(data));
+//       visitorScoring.innerHTML(`${data.score}`);
+//       // data.forEach((x) => {
+//       //   console.log(data);
+//       //   visitorScoring.innerHTML(`<h2>${x.score}</h2>`);
+//     });
 // };
 
-// getScores = async (x) => {
-//   const grabScore = await fetch("http://localhost:3500/visitorScore").then(
-//     (response) => response.json()
-//   );
-//   visitorScoring.innerHTML = grabScore.map(x) => {
-//     JSON.stringify(x.score)
-//     return response;
-//   };
-// };
-
-// return (visitorScoring.innerHTMl = await grabScore.map((x) => {
-//   return JSON.stringify(x.score);
-// }
-
-// const scoring = (getScores = async (x) => {
-//   const grabScore = await fetch("http://localhost:3500/visitorScore").then(
-//     (response) => response.json()
-//   );
-//   return (visitorScoring.innerHTMl = grabScore.map((x) => {
-//     return JSON.stringify(x.score);
-//   }));
-// });
-
-// return visitorScore.append.(grabScore.score).innerHTML;
-
-// const async getScores = () => {
-//   return (visitorScore.innerHTML = await fetch(
-//     "http://localhost:3500/visitorScore"
-//   ).then((response) => response.json()));
-// };
-
-getScores();
-
-// async function getScore() {
-//   try {
-//     const response = await fetch(API_URL);
-//     const listScore = await response.json();
-//     setItems(listScore);
-//     console.log(listScore);
-//   } catch (error) {
-//     console.log(error.stack);
-//   }
-//   async () => await getScore();
-// }
-// [];
-
-// const currentVisitorScore =
-//   document.getElementById("visitor-score-points").innerHTML;
-// console.log(currentVisitorScore);
-
-// const currentScore = {
-//   score: 1,
-//   logScore: function () {
-//     console.log(this.score);
-//   },
-// };
+// getScores();
